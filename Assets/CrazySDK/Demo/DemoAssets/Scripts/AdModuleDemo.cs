9cf77a3448167ba1c2dc8080260a7fbf3ccdbdac
Mayur <mayur.saitwal@gmail.com>
2025-09-20 16:42:46 +0530

    no message

---
+++ Assets/CrazySDK/Demo/DemoAssets/Scripts/AdModuleDemo.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace CrazyGames
+{
+    public class AdModuleDemo : MonoBehaviour
+    {
+        public Text timerText;
+
+        private void Start()
+        {
+            CrazySDK.Init(() => { }); // ensure if starting this scene from editor it is initialized
+        }
+
+        private void Update()
+        {
+            timerText.text = "Timer: " + Time.time;
+
+            if (Input.GetKeyDown(KeyCode.M))
+            {
+                ShowMidgameAd();
+            }
+
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                ShowRewardedAd();
+            }
+
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+        }
+
+        public void ShowMidgameAd()
+        {
+            CrazySDK.Ad.RequestAd(
+                CrazyAdType.Midgame,
+                () =>
+                {
+                    Debug.Log("Midgame ad started");
+                },
+                (error) =>
+                {
+                    Debug.Log("Midgame ad error: " + error);
+                },
+                () =>
+                {
+                    Debug.Log("Midgame ad finished");
+                }
+            );
+        }
+
+        public void ShowRewardedAd()
+        {
+            CrazySDK.Ad.RequestAd(
+                CrazyAdType.Rewarded,
+                () =>
+                {
+                    Debug.Log("Rewarded ad started");
+                },
+                (error) =>
+                {
+                    Debug.Log("Rewarded ad error: " + error);
+                },
+                () =>
+                {
+                    Debug.Log("Rewarded ad finished, reward the player here");
+                }
+            );
+        }
+    }
+}
---
+++ Assets/CrazySDK/Demo/DemoAssets/Scripts/UserModuleDemo.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+namespace CrazyGames
+{
+    public class UserModuleDemo : MonoBehaviour
+    {
+        public Text userAccountAvailableText;
+        public Text userLoggedOutText;
+        public Image userAvatarImage;
+        public Text usernameText;
+        public GameObject userInfoContainer;
+
+        private readonly List<Action<PortalUser>> _authListeners = new List<Action<PortalUser>>();
+
+        public void Start()
+        {
+            userInfoContainer.SetActive(false);
+            userLoggedOutText.gameObject.SetActive(false);
+            CrazySDK.Init(() =>
+            {
+                CrazySDK.User.GetUser(
+                    (user) =>
+                    {
+                        if (user != null)
+                        {
+                            Debug.Log("User is signed in: " + user);
+                            userInfoContainer.SetActive(true);
+                            usernameText.text = user.username;
+                            StartCoroutine(FetchUserImage(user.profilePictureUrl));
+                        }
+                        else
+                        {
+                            Debug.Log("User is not signed in");
+                            userLoggedOutText.gameObject.SetActive(true);
+                        }
+                    }
+                );
+            }); // ensure if starting this scene from editor it is initialized
+
+            if (userAccountAvailableText != null)
+            {
+                var isAvailable = CrazySDK.User.IsUserAccountAvailable;
+                userAccountAvailableText.text = $"User account available: {isAvailable}";
+            }
+        }
+
+        IEnumerator FetchUserImage(string url)
+        {
+            UnityWebRequest request = UnityWebRequestTexture.GetTexture(url);
+            yield return request.SendWebRequest();
+
+            if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
+            {
+                Debug.LogError(request.error);
+            }
+            else
+            {
+                Texture2D texture = DownloadHandlerTexture.GetContent(request);
+                userAvatarImage.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
+            }
+        }
+
+        public void GetUser()
+        {
+            CrazySDK.User.GetUser(
+                (user) =>
+                {
+                    if (user != null)
+                    {
+                        Debug.Log("Get user result: " + user);
+                    }
+                    else
+                    {
+                        Debug.Log("User is not signed in");
+                    }
+                }
+            );
+        }
+
+        public void GetToken()
+        {
+            CrazySDK.User.GetUserToken(
+                (error, token) =>
+                {
+                    if (error != null)
+                    {
+                        Debug.LogError("Get user token error: " + error);
+                        return;
+                    }
+
+                    Debug.Log("User token: " + token);
+                }
+            );
+        }
+
+        public void GetXsollaUserToken()
+        {
+            CrazySDK.User.GetXsollaUserToken(
+                (error, token) =>
+                {
+                    if (error != null)
+                    {
+                        Debug.LogError("Get Xsolla user token error: " + error);
+                        return;
+                    }
+
+                    Debug.Log("Xsolla user token: " + token);
+                }
+            );
+        }
+
+        public void SyncUnityGameData()
+        {
+            CrazySDK.User.SyncUnityGameData();
+        }
+
+        public void ShowAuthPrompt()
+        {
+            CrazySDK.User.ShowAuthPrompt(
+                (error, user) =>
+                {
+                    if (error != null)
+                    {
+                        Debug.LogError("Show auth prompt error: " + error);
+                        return;
+                    }
+
+                    Debug.Log("Auth prompt user: " + user);
+                }
+            );
+        }
+
+        public void ShowAccountLinkPrompt()
+        {
+            CrazySDK.User.ShowAccountLinkPrompt(
+                (error, answer) =>
+                {
+                    if (error != null)
+                    {
+                        Debug.LogError("Show account link prompt error: " + error);
+                        return;
+                    }
+
+                    Debug.Log("Account link answer: " + answer);
+                }
+            );
+        }
+
+        public void AddAuthListener()
+        {
+            _authListeners.Add(
+                (user) =>
+                {
+                    Debug.Log("Auth listener, user: " + user);
+                }
+            );
+            CrazySDK.User.AddAuthListener(_authListeners.Last());
+        }
+
+        public void RemoveLastAuthListener()
+        {
+            if (_authListeners.Count == 0)
+            {
+                return;
+            }
+
+            var lst = _authListeners.Last();
+            CrazySDK.User.RemoveAuthListener(lst);
+            _authListeners.Remove(lst);
+        }
+
+        public void GetSystemInfo()
+        {
+            var systemInfo = CrazySDK.User.SystemInfo;
+            Debug.Log($"All system info: {systemInfo}");
+            Debug.Log($"Browser: {systemInfo.browser}");
+            Debug.Log($"OS: {systemInfo.os}");
+            Debug.Log($"Country code: {systemInfo.countryCode}");
+            Debug.Log($"Device type: {systemInfo.device.type}");
+        }
+
+        public void AddScore()
+        {
+            CrazySDK.User.AddScore(152.1f);
+        }
+    }
+}
